66a72ead83e15f126110f8082f3cd18af1882fb8
sparkvilla <diego.villamaina@gmail.com>
2025-01-10 14:58:09 +0100

    added logo description

--- home.py
+++ home.py
@@ -1,12 +1,15 @@
 import streamlit as st
 from streamlit_folium import st_folium
-from utils import (load_css, resize_image,
-                   get_base64_image, 
-                   read_json_file, 
-                   create_map,
-                   display_project_details_below_map,
-                   filter_projects, 
-                   display_project_list)
+from utils import (
+    load_css,
+    resize_image,
+    get_base64_image,
+    read_json_file,
+    create_map,
+    display_project_details_below_map,
+    filter_projects,
+    display_project_list,
+)
 from pages.biodiversity import biodiversity_page
 
 LOGOS_DIR = "logos_resized"
@@ -14,18 +17,32 @@ CSS_FILE = "static/styles.css"
 DATA_FILE = "ess_proj_data_loc_02.json"
 
 TOPIC_INFO = {
-    "Biodiversity": {"image": "topics_resized/biodiversity_img.jpg", "page": "biodiversity"},
-    "Climate Change": {"image": "topics_resized/climate_change_img.png", "page": "climate_change"},
-    "Ecosystem Services": {"image": "topics_resized/ecosystem_services_img.jpg", "page": "ecosystem_services"},
-    "Sustainable Development": {"image": "topics_resized/sus_dev_img.png", "page": "sustainable_dev"},
+    "Biodiversity": {
+        "image": "topics_resized/biodiversity_img.jpg",
+        "page": "biodiversity",
+    },
+    "Climate Change": {
+        "image": "topics_resized/climate_change_img.png",
+        "page": "climate_change",
+    },
+    "Ecosystem Services": {
+        "image": "topics_resized/ecosystem_services_img.jpg",
+        "page": "ecosystem_services",
+    },
+    "Sustainable Development": {
+        "image": "topics_resized/sus_dev_img.png",
+        "page": "sustainable_dev",
+    },
 }
+
+
 def home_page():
-    title = '<p style="font-family:sans-serif; color:White; font-size: 42px; font-weight:bold;">Earth System Science</p>'
+    title = '<p style="font-family:sans-serif; color:Black; font-size: 42px; font-weight:bold;">Earth System Science</p>'
     st.markdown(title, unsafe_allow_html=True)
     # Topics Section
     st.markdown("---")
     st.markdown("## Main Research Topics")
-    
+
     cols = st.columns(len(TOPIC_INFO))
 
     for idx, (topic, info) in enumerate(TOPIC_INFO.items()):
@@ -33,32 +50,12 @@ def home_page():
             resized_image = resize_image(info["image"], width=150, height=130)
             st.image(resized_image)
             st.markdown(f"{topic}")
-            #if st.button(f"Explore {topic}", key=f"button_{topic}"):
+            # if st.button(f"Explore {topic}", key=f"button_{topic}"):
             #    st.markdown(f"[Explore {topic}]({info['page']})", unsafe_allow_html=True)
 
     st.markdown("---")
-    
-    #background_image_css = f"""
-    # <style>
-    # .stApp {{
-    #    background: url('{get_base64_image("logos/biodiv_background.jpeg")}') no-repeat center center fixed;
-    #    background-size: cover;
-    # }}
-    # .stMainBlockContainer {{
-    #    overflow: visible !important; /* Ensure content fits without clipping */
-    #    height: auto !important;     /* Adjust to the content height */
-    # }}
-    # .leaflet-container {{
-    #    background: transparent !important; /* Fix map background */
-    # }}
-    # </style>
-    # """
 
     locations = read_json_file(DATA_FILE)
-
-    ### Streamlit App Layout
-    #st.markdown(background_image_css, unsafe_allow_html=True)
-
 
     ## Sidebar search functionality
     st.sidebar.markdown("## Filter Projects")
@@ -93,8 +90,6 @@ def home_page():
         display_project_details_below_map(selected_project, LOGOS_DIR)
 
 
-
-
 # Main Application
 def main():
     print("start")
@@ -108,13 +103,13 @@ def main():
         home_page()
     elif st.session_state["current_page"] == "biodiversity":
         biodiversity_page()
-    #elif st.session_state["current_page"] == "climate_change":
+    # elif st.session_state["current_page"] == "climate_change":
     #    climate_change_page()
-    #elif st.session_state["current_page"] == "ecosystem_services":
+    # elif st.session_state["current_page"] == "ecosystem_services":
     #    ecosystem_services_page()
-    #elif st.session_state["current_page"] == "sustainable_dev":
+    # elif st.session_state["current_page"] == "sustainable_dev":
     #    sustainable_dev_page()
-    
+
     # Use the encoded image in CSS
     print("end")
     print("---")
--- utils.py
+++ utils.py
@@ -5,23 +5,28 @@ from PIL import Image
 import streamlit as st
 import folium
 
+
 def load_css(css_file):
     with open(css_file) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
+
 def resize_image(image_path, width, height):
     image = Image.open(image_path)
     return image.resize((width, height))
+
 
 def get_base64_image(image_path):
     with open(image_path, "rb") as file:
         encoded = base64.b64encode(file.read()).decode()
     return f"data:image/png;base64,{encoded}"
 
+
 def read_json_file(json_file):
     with open(json_file, "r") as file:
         data = json.load(file)
     return data["locations"]
+
 
 def create_map(locations, center=None, zoom=1):
     map_center = center if center else [30.079227, -21.750656]
@@ -44,10 +49,15 @@ def create_map(locations, center=None, zoom=1):
 def display_project_details_below_map(location, logos_dir):
     """Display selected project details below the map."""
     if location:
+
         st.markdown("---")
         st.markdown(f"### {location['project']}")
         st.markdown(f"**Participants:**")
         st.markdown(", ".join(location["participants"]))
+
+        # Add extra space between sections
+        st.markdown("&nbsp;", unsafe_allow_html=True)
+
         st.markdown(f"**Description:** {location['description']}")
         if location["doi_data"]:
             st.markdown(f"[DOI Data]({location['doi_data']})")
@@ -56,11 +66,25 @@ def display_project_details_below_map(location, logos_dir):
         if location["website"]:
             st.markdown(f"[More info]({location['website']})")
 
+        # Add extra space between sections
+        st.markdown("&nbsp;", unsafe_allow_html=True)
+
         st.markdown("**Funding Organizations:**")
         for logo in location["logos"]:
-            logo_path = os.path.join(logos_dir, logo)
+            logo_name, logo_description = logo
+            logo_path = os.path.join(logos_dir, logo_name)
             resized_logo = resize_image(logo_path, width=110, height=80)
-            st.image(resized_logo)
+            col1, col2 = st.columns([1, 3])
+            with col1:
+                st.image(resized_logo)
+            with col2:
+                # Add extra space between sections
+                st.markdown("&nbsp;", unsafe_allow_html=True)
+                st.markdown(logo_description)
+
+        # Add extra space between sections
+        st.markdown("&nbsp;", unsafe_allow_html=True)
+
         st.markdown("**Tags:**")
         tags_html = " ".join(
             f"<span class='tag'>{tag}</span>" for tag in location["tags"]
@@ -69,7 +93,7 @@ def display_project_details_below_map(location, logos_dir):
 
         st.markdown("---")
 
-    
+
 def handle_map_click(output, locations):
     if output["last_object_clicked"]:
         clicked_lat = round(output["last_object_clicked"]["lat"], 4)
